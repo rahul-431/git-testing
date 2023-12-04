08bdaecf4392b33f519e88535e39fec64e3e9fcb
Rahul Mijar <rahul.mijar@trunary.com>
2023-12-04 11:07:33 +0545

    setting the main file to execute the game configuration class

--- SingletonDemo/GameConfigurationManger.cs
+++ SingletonDemo/GameConfigurationManger.cs
@@ -1,16 +1,13 @@
 
-
 // ------ THIS IS THE DEMO FOR SINGLETON CLASS
-
-
-
 using System;
 using System.ComponentModel;
 using System.Reflection.Emit;
-namespace DesignPattern{
+namespace DesignPattern
+{
     public class GameConfigurationManger
     {
-        private Dictionary<string,string> configurationSetting;
+        private Dictionary<string, string> configurationSetting;
         private static GameConfigurationManger instance;
 
         // private Constructor for instantiation of configuration setting
@@ -22,17 +19,19 @@ namespace DesignPattern{
         // for creating the single instance of GameConfigurationManger class;
         public static GameConfigurationManger Instance
         {
-            get{
-                if(instance==null)
+            get
             {
-                instance=new GameConfigurationManger();
-            }
-            return instance;
+                if (instance == null)
+                {
+                    instance = new GameConfigurationManger();
+                }
+                return instance;
             }
         }
         //method for getting configuration setting
-        public string getConfigurationSetting(string key){
-            if(configurationSetting.ContainsKey(key))
+        public string getConfigurationSetting(string key)
+        {
+            if (configurationSetting.ContainsKey(key))
             {
                 return configurationSetting[key];
             }
@@ -41,15 +40,31 @@ namespace DesignPattern{
         //method for setting and updating a new configuration setting;
         public void setConfigurationSettting(string key, string value)
         {
-            if(configurationSetting.ContainsKey(key))
+            if (configurationSetting.ContainsKey(key))
             {
-                configurationSetting[key]=value;
+                configurationSetting[key] = value;
             }
-            else{
-            configurationSetting.Add(key,value); 
+            else
+            {
+                configurationSetting.Add(key, value);
             }
-                
-        }
 
-    }   
+        }
+        public void getAllList()
+        {
+            if (configurationSetting == null)
+            {
+                Console.WriteLine("Nothig to show");
+            }
+            else
+            {
+                foreach (KeyValuePair<string, string> conf in configurationSetting)
+                {
+                    Console.WriteLine("-------These are the list of all configuration setting------");
+                    Console.WriteLine($"{conf.Key}:{conf.Value}");
+                }
+            }
+
+        }
+    }
 }
